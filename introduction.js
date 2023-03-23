bc5837b34ccdfd93b6ab993470dd46d58ff7d2d2
Alex Gorichev <Alex.Gorichev@protonmail.com>
2023-03-23 17:39:27 +0000

    Course: add D

--- introduction.js
+++ introduction.js
@@ -1,13 +1,18 @@
 async function start() {
 	removeCSSfile('pre-playing.css')
 	await sleep(500)
+	await middleC()
+	await D()
+}
+
+async function middleC() {
 	const middleC = displayText('This is middle C')
 	await sleep(200)
 	await playNote(60, 800)
 	await hideText(middleC)
 	const theirAttempt = displayText('Now you try')
 	if (!(await expectNote(60))) {
-		const notQuite = displayText('Not quite', 1)
+		displayText('Not quite', 1)
 		await hideText(theirAttempt)
 		let tryAgain = displayText('Try again')
 		while (!(await expectNote(60))) {
@@ -16,5 +21,28 @@ async function start() {
 		}
 	}
 	await clearText()
-	displayText('Very nice', 1)
+	displayText('Very nice')
+	await sleep(1200)
+	await clearText()
+}
+
+async function D() {
+	const middleC = displayText('This is the D above middle C')
+	await sleep(200)
+	await playNote(62, 800)
+	await hideText(middleC)
+	const theirAttempt = displayText('Now you try')
+	if (!(await expectNote(62))) {
+		displayText('Not quite', 1)
+		await hideText(theirAttempt)
+		let tryAgain = displayText('Try again')
+		while (!(await expectNote(62))) {
+			await hideText(tryAgain)
+			tryAgain = displayText('Try again')
+		}
+	}
+	await clearText()
+	displayText('Good good')
+	await sleep(1200)
+	await clearText()
 }
