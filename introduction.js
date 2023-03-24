b9bfe5e4298c331441f7f72b724a32ccf63203fe
Alex Gorichev <Alex.Gorichev@protonmail.com>
2023-03-24 07:27:25 +0000

    DRY course code

--- introduction.js
+++ introduction.js
@@ -1,71 +1,30 @@
 async function start() {
 	removeCSSfile('pre-playing.css')
 	await sleep(500)
-	await middleC()
-	await D()
-	await together()
+	await basicPlaying('This is middle C', [60])
+	await basicPlaying('This is the D above middle C', [62])
+	await basicPlaying('Now together', [60, 62])
 }
 
-async function middleC() {
-	const middleC = displayText('This is middle C')
+async function basicPlaying(text, notes) {
+	const thisIsD = displayText(text)
 	await sleep(200)
-	await playNote(60, 800)
-	await hideText(middleC)
-	const theirAttempt = displayText('Now you try')
-	if (!(await expectNote(60))) {
-		displayText('Not quite', 1)
-		await hideText(theirAttempt)
-		let tryAgain = displayText('Try again')
-		while (!(await expectNote(60))) {
-			await hideText(tryAgain)
-			tryAgain = displayText('Try again')
-		}
+	for (const note of notes) {
+		await playNote(note, 800)
 	}
-	await clearText()
-	displayText('Very nice')
-	await sleep(1200)
-	await clearText()
-}
-
-async function D() {
-	const thisIsD = displayText('This is the D above middle C')
-	await sleep(200)
-	await playNote(62, 800)
 	await hideText(thisIsD)
 	const theirAttempt = displayText('Now you try')
-	if (!(await expectNote(62))) {
+	if (!(await expectSequence(notes))) {
 		displayText('Not quite', 1)
 		await hideText(theirAttempt)
 		let tryAgain = displayText('Try again')
-		while (!(await expectNote(62))) {
+		while (!(await expectSequence(notes))) {
 			await hideText(tryAgain)
 			tryAgain = displayText('Try again')
 		}
 	}
 	await clearText()
 	displayText('Good good')
-	await sleep(1200)
-	await clearText()
-}
-
-async function together() {
-	const nowTogether = displayText('Now together')
-	await sleep(100)
-	await playNote(60, 800)
-	await playNote(62, 800)
-	await hideText(nowTogether)
-	const theirAttempt = displayText('Now you try')
-	if (!(await expectSequence([60, 62]))) {
-		displayText('Not quite', 1)
-		await hideText(theirAttempt)
-		let tryAgain = displayText('Try again')
-		while (!(await expectSequence([60, 62]))) {
-			await hideText(tryAgain)
-			tryAgain = displayText('Try again')
-		}
-	}
-	await clearText()
-	displayText('Nicee')
 	await sleep(1200)
 	await clearText()
 }
